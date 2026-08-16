d7e60945f8eeab527890922b97e99369ffe30f5c
vxaxcxa <valerijagocic@gmail.com>
2026-08-16 18:58:57 +0200

    feat: dodata glavna Spring Boot aplikacija

---
+++ src/main/java/rs/ac/metropolitan/it355/salon/SalonApplication.java
@@ -0,0 +1,16 @@
+package rs.ac.metropolitan.it355.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ulazna tacka aplikacije "Sistem za rezervaciju termina u kozmetickom salonu".
+ * IT355 - Web sistemi 2, prvi predispitni projektni zadatak.
+ */
+@SpringBootApplication
+public class SalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonApplication.class, args);
+    }
+}
